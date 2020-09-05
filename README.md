7b6c0cac389f83514ca3173395732bde3b3d8588
Carlos Figueiredo <cvieirasp@gmail.com>
2020-09-05 09:50:14 -0300

    Primeira versão da Aplicação

---
+++ README.md
@@ -0,0 +1,5 @@
+## Repo Backend Node
+
+Aplicação criada para armazenar repositórios de portfólio, que permite a criação, listagem, atualização e remoção dos repositórios. Além disso, também permite que os repositórios possam receber "likes".
+
+*Aplicação criada como primeiro desafio do bootcamp GoStack (Nível 1)*
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +9,115 @@ app.use(cors());
 
 const repositories = [];
 
+function getIndex(id) {
+  return repositories.findIndex(repo => repo.id === id);
+}
+
+function validateInsert(request, response, next) {
+  const { title, url, techs } = request.body;
+
+  if (!title) {
+    return response.status(400).json({ message: 'Campo Titulo obrigatorio!' });
+  }
+
+  if (!url) {
+    return response.status(400).json({ message: `Campo URL obrigatório!` });
+  }
+
+  return next();
+}
+
+function validateId(request, response, next) {
+  const { id } = request.params;
+  
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: 'ID do Repositorio invalido!' });
+  }
+
+  return next();
+}
+
+app.use("/repositories/:id", validateId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const index = getIndex(id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: 'Repositorio nao encontrado!' });
+  }
+
+  const repo = repositories[index];
+
+  return response.json(repo);
+});
+
+app.post("/repositories", validateInsert, (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = getIndex(id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: 'Repositorio nao encontrado!' });
+  }
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[index].likes,
+  }
+
+  repositories[index] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = getIndex(id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: 'Repositorio nao encontrado!' });
+  }
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = getIndex(id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: 'Repositorio nao encontrado!' });
+  }
+
+  const repo = repositories[index];
+  repo.likes++;
+  return response.json({ likes: repo.likes});
 });
 
 module.exports = app;
